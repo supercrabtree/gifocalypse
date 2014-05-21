e716f10b10db9c544a1e687cfd2c8695d6c6100b
George Crabtree <supercrabtree@googlemail.com>
2014-05-21 11:19:36 +1000

    loading first gif

--- cat-gif-saver/cat_gif_saverView.m
+++ cat-gif-saver/cat_gif_saverView.m
@@ -4,7 +4,7 @@
 //
 //  Created by George Crabtree on 21/05/2014.
 //  Copyright (c) 2014 George Crabtree. All rights reserved.
-//
+//  http://giphy.com/embed/7z2oyDXIMEs8w
 
 #import "cat_gif_saverView.h"
 
@@ -12,6 +12,7 @@
 
 - (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
 {
+    NSLog(@"George");
     self = [super initWithFrame:frame isPreview:isPreview];
     if (self) {
         [self setAnimationTimeInterval:1/30.0];
@@ -21,6 +22,9 @@
 
 - (void)startAnimation
 {
+    NSURL *imageURL = [NSURL URLWithString:@"http://giphy.com/embed/7z2oyDXIMEs8w"];
+    NSImage *image = [[NSImage alloc] initWithContentsOfURL:imageURL];
+    NSLog(@"size %f %f",image.size.width, image.size.height);
     [super startAnimation];
 }
 
@@ -31,11 +35,27 @@
 
 - (void)drawRect:(NSRect)rect
 {
-    [super drawRect:rect];
+//    [super drawRect:rect];
 }
 
 - (void)animateOneFrame
 {
+//    NSImage *image;
+//    NSRect myRect;
+//    NSSize mySize;
+//    
+//    image = [[NSImage imageNamed:@"mypict.jpg"] copy];
+//    
+//    mySize = [image size];
+//    
+//    myRect.origin.x = 0;
+//    myRect.origin.y = 0;
+//    myRect.size = mySize;
+//    
+//    [image lockFocus];
+//    [image drawInRect:[self bounds] fromRect:myRect
+//            operation:NSCompositeCopy fraction:0.5];
+//    [image unlockFocus];
     return;
 }
 
