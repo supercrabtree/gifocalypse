c15ccd51f67de59743ae6b57696e20787eed2fda
George Crabtree <supercrabtree@googlemail.com>
2014-05-22 09:19:25 +1000

    moved image loading in a separte folder

--- cat-gif-saver/cat_gif_saverView.m
+++ cat-gif-saver/cat_gif_saverView.m
@@ -21,10 +21,9 @@
 
 - (void)startAnimation
 {
+    [super startAnimation];
     NSURL *url = [NSURL URLWithString:@"http://giphy.com/embed/7z2oyDXIMEs8w"];
     image = [[NSImage alloc] initWithContentsOfURL:url];
-    
-    [super startAnimation];
 }
 
 - (void)stopAnimation
@@ -34,6 +33,11 @@
 
 - (void)drawRect:(NSRect)rect
 {
+    NSRect imageRect;
+    imageRect.origin = NSZeroPoint;
+    imageRect.size = [image size];
+    NSRect drawingRect = imageRect;
+    [image drawInRect:drawingRect fromRect:imageRect operation:NSCompositeSourceOver fraction:1];
     [image drawInRect:rect];
     [super drawRect:rect];
 }
